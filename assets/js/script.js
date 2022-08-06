665939b11cea3812227fa5229ff00697132acac5
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-08-06 15:18:57 -0300

    finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.querySelector(".menu-opener").addEventListener('click', function() {
+        document.querySelector("nav").classList.toggle('abri')
+    })
+}
